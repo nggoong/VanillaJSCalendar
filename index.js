8253d66013cd08322834c4374af55e3efbc24a1e
nggoong <nggoong@naver.com>
2022-02-28 21:15:08 +0900

    현재 년도, 달을 가져와 렌더링하고, 버튼 클릭 시 이전 달, 다음 달로 이동하는 기능 구현(달력 내부 날짜가 바뀌는 기능은 추후 구현 예정)

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
 (()=> {
-    console.log('hello');
+    let dateObj;
+    let currentYear;
+    let currentMonth;
+
+    
+
+    const modifyNumber = (num) => { 
+        if(num < 10) return '0' + num
+        else return num;
+    }
+
+    const initYearMonth = () => {
+        dateObj = new Date();
+        currentYear = dateObj.getFullYear();
+        currentMonth = modifyNumber(dateObj.getMonth() + 1);
+    }
+
+    const RenderMonthYear = (year, month) => {
+        let monthArea = document.querySelector('.month');
+        monthArea.innerHTML=`${year}.${month}`;
+    }
+
+    const moveNextMonth = () => {
+        if(currentMonth == 12){
+            currentMonth = 1;
+            currentYear = parseInt(currentYear) + 1;
+        } 
+        else {
+            currentMonth = parseInt(currentMonth) + 1;
+        }
+
+        RenderMonthYear(modifyNumber(currentYear), modifyNumber(currentMonth));
+        
+    }
+
+    const movePrevMonth = () => {
+        if(currentMonth == 1) {
+            currentMonth = 12;
+            currentYear = parseInt(currentYear) - 1;
+        }
+        else {
+            currentMonth = parseInt(currentMonth) - 1;
+        }
+        RenderMonthYear(modifyNumber(currentYear), modifyNumber(currentMonth));
+    }
+
+    const setButtonEvent = () => {
+        prevBtn = document.querySelector('.prev');
+        nextBtn = document.querySelector('.next');
+
+        nextBtn.addEventListener('click', moveNextMonth);
+        prevBtn.addEventListener('click', movePrevMonth);
+    }
+
+
+
+
+
+
+
+
+    initYearMonth();
+    setButtonEvent();
+    RenderMonthYear(currentYear, currentMonth);
+
+    
+    
+
+
+    
 })();
